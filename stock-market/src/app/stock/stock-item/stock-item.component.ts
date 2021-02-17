4b772ccb099eb447a908c2af499cb8eeea1d43d5
Alex LeFevre <alexander.m.lefevre@gmail.com>
2021-02-17 15:23:24 -0500

    OR 21

---
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css'],
+})
+export class StockItemComponent implements OnInit {
+  public name: string | undefined;
+  public code: string | undefined;
+  public price: number | undefined;
+  public previousPrice: number | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+  }
+}
